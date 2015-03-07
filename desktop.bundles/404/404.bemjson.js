3aa27e69357e844b2cfece7efa4990bef6deac3f
Pavel Bashinsky <pbashinsky@yandex-team.ru>
2015-03-08 02:32:47 +0300

    Add page 404

---
+++ desktop.bundles/404/404.bemjson.js
@@ -0,0 +1,12 @@
+({
+    block: 'page',
+    title: '404 Not Found',
+    mods: { error: 'on' },
+    content: [
+       {
+           block: 'error',
+           title: '404 Not Found',
+           content: 'bender/4.0.4'
+       }
+    ]
+})
